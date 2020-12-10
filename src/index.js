faba8d1133fdd3b69e83c9470f2c2e3fe33f5fdc
Kevin McCallum <kevingmcc@gmail.com>
2020-12-10 12:41:54 -0700

    Created a counter component with dark mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Counter extends Component {
+  state = {
+    wrap: {
+      count: 0,
+      lit: true,
+    },
+  }
+
+  increment = () => {
+    this.setState({
+      wrap: {
+        ...this.state.wrap,
+        count: (this.state.wrap.count += 1),
+      },
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      wrap: {
+        ...this.state.wrap,
+        count: (this.state.wrap.count -= 1),
+      },
+    })
+  }
+
+  toggle = () => {
+    this.setState((prev) => ({
+      wrap: {
+        ...prev.wrap,
+        lit: !prev.wrap.lit,
+      },
+    }))
+  }
+
+  render() {
+    const { count, lit } = this.state.wrap
+    return (
+      <div className={`counter ${lit ? '' : 'dark'}`}>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span className="count">{count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+        <button onClick={this.toggle}>On / Off</button>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
